401658c34056f23c8498907febcf89a809a9fb8e
TheNuc1ear <86493432+TheNuc1ear@users.noreply.github.com>
2022-02-23 20:29:57 +0800

    Edited login page and name of app

--- app/src/main/java/com/example/myfirstapp/LoginActivity.java
+++ app/src/main/java/com/example/myfirstapp/LoginActivity.java
@@ -1,13 +1,47 @@
 package com.example.myfirstapp;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout textUsernameLayout;
+    private TextInputLayout textPasswordInput;
+    private Button loginButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textUsernameLayout = findViewById(R.id.textUsernameLayout);
+        textPasswordInput = findViewById(R.id.textPasswordInput);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.onLoginClicked();
+            }
+        });
     }
+
+    private void onLoginClicked() {
+        String username = textUsernameLayout.getEditText().getText().toString();
+        String password = textPasswordInput.getEditText().getText().toString();
+        if (username.isEmpty()) {
+            textUsernameLayout.setError("Username must not be empty");
+        } else if (password.isEmpty()) {
+            textPasswordInput.setError("Password must not be empty");
+            // Textwatcher can be used to get rid of error message when user fills it up again
+        }
+    }
+
 }
