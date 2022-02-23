17dc97b530b10f49ace7662c74c0c220cf33276c
TheNuc1ear <86493432+TheNuc1ear@users.noreply.github.com>
2022-02-23 23:07:49 +0800

    Link to Signup from Login

--- app/src/main/java/com/infosys/b4b/LoginActivity.java
+++ app/src/main/java/com/infosys/b4b/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.infosys.b4b;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputLayout textUsernameLayout;
     private TextInputLayout textPasswordInput;
     private Button loginButton;
+    private Button signupButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,10 +25,18 @@ public class LoginActivity extends AppCompatActivity {
         textUsernameLayout = findViewById(R.id.textUsernameLayout);
         textPasswordInput = findViewById(R.id.textPasswordInput);
         loginButton = findViewById(R.id.loginButton);
+        signupButton = findViewById(R.id.signupButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LoginActivity.this.onLoginClicked();
+            }
+        });
+        signupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
             }
         });
     }
