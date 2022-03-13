48093c64c228c594a7d70eb12478f59b815bba0f
haydenang <64570131+haydenang@users.noreply.github.com>
2022-03-13 12:32:25 +0800

    UI Addition
    -Added Splash Screen Function, launches with splashscreentheme, after mainactivity loads, switches to mainactivitytheme
    -Added B4B Icon

--- app/src/main/java/com/infosys/b4b/MainActivity.java
+++ app/src/main/java/com/infosys/b4b/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_MyFirstApp);
         setContentView(R.layout.activity_main);
 
         btnLogOut = findViewById(R.id.btnLogout);
