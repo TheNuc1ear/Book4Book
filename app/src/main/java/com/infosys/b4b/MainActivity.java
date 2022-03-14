c4b718c919b014b3c7c4598b2bbb91f2532c6d13
haydenang <64570131+haydenang@users.noreply.github.com>
2022-03-15 02:06:19 +0800

    Small additions to MainActivity
    Added more assets for activity_main

--- app/src/main/java/com/infosys/b4b/MainActivity.java
+++ app/src/main/java/com/infosys/b4b/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SearchView;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnLogOut;
     FirebaseAuth mAuth;
+    ImageButton chatBox;
+    SearchView searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +25,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogOut = findViewById(R.id.btnLogout);
+        chatBox = findViewById(R.id.chatBox);
+        searchBar = findViewById(R.id.searchBar);
         FirebaseApp.initializeApp(getApplicationContext());
         mAuth = FirebaseAuth.getInstance();
 
         btnLogOut.setOnClickListener(view ->{
             mAuth.signOut();
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
+        chatBox.setOnClickListener(view->{
+            //TODO: Change to ChatActivity/activity_chat
+        });
+
+        searchBar.setOnClickListener(view->{
+            //TODO: Change to a RecyclerView for books
         });
 
     }
