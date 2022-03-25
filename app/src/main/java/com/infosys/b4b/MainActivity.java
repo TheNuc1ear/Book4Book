26f80ac62f1172da8ddf2b72bca26482b47561c3
haydenang <64570131+haydenang@users.noreply.github.com>
2022-03-25 18:43:58 +0800

    Added Filter Function
    Filter Function has been added to the RecyclerView, will update the listing shown real-time as the user types in the searchbar

--- app/src/main/java/com/infosys/b4b/MainActivity.java
+++ app/src/main/java/com/infosys/b4b/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView bookList;
     List<String> bookTitles;
     List<Integer> bookImages;
+    List<bookListing> allBookListing;
     Adapter adapter;
 
     @Override
@@ -37,29 +41,33 @@ public class MainActivity extends AppCompatActivity {
         btnLogOut = findViewById(R.id.btnLogout);
         chatBox = findViewById(R.id.chatBox);
         searchBar = findViewById(R.id.searchBar);
-        bookList = findViewById(R.id.bookList);
         FirebaseApp.initializeApp(getApplicationContext());
         mAuth = FirebaseAuth.getInstance();
         bookTitles = new ArrayList<>();
         bookImages = new ArrayList<>();
+        allBookListing = new ArrayList<>();
         //Convert database of book with picture and title to list of title/pics
-        bookTitles.add("Bro");
-        bookImages.add(R.drawable.midnight_lib);
-        bookTitles.add("Sis");
-        bookImages.add(R.drawable.giannis);
-        bookTitles.add("Mom");
-        bookImages.add(R.drawable.giannis);
-        bookTitles.add("Dad");
-        bookImages.add(R.drawable.midnight_lib);
-        bookTitles.add("Gma");
-        bookImages.add(R.drawable.giannis);
-        bookTitles.add("Gpa");
-        bookImages.add(R.drawable.midnight_lib);
-        //Initialise Adapter
-        adapter = new Adapter(this,bookTitles,bookImages);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
-        bookList.setLayoutManager(gridLayoutManager);
-        bookList.setAdapter(adapter);
+        allBookListing.add(new bookListing("Giannis",R.drawable.giannis));
+        allBookListing.add(new bookListing("Midnight Library",R.drawable.midnight_lib));
+        allBookListing.add(new bookListing("Giannis",R.drawable.giannis));
+        allBookListing.add(new bookListing("Midnight Library",R.drawable.midnight_lib));
+        allBookListing.add(new bookListing("Giannis",R.drawable.giannis));
+        allBookListing.add(new bookListing("Midnight Library",R.drawable.midnight_lib));
+        //Initialise RecyclerView
+        setUpRecyclerView();
+
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s); //s is whatever the user type into the searchview
+                return true;
+            }
+        });
 
         btnLogOut.setOnClickListener(view ->{
             mAuth.signOut();
@@ -84,6 +92,14 @@ public class MainActivity extends AppCompatActivity {
         if (user == null){                                                                  // Redirect to login page
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
         }
+    }
+
+    private void setUpRecyclerView(){
+        bookList = findViewById(R.id.bookList);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        adapter = new Adapter(this,allBookListing);
+        bookList.setLayoutManager(gridLayoutManager);
+        bookList.setAdapter(adapter);
     }
 }
 
