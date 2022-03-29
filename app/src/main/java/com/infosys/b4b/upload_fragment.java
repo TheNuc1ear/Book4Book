fb5674d2ce5bfdb621662881603e812d4aaa51ed
anirhc <anirhc@gmail.com>
2022-03-29 16:10:23 +0800

    Upload Book Fragment

--- app/src/main/java/com/infosys/b4b/upload_fragment.java
+++ app/src/main/java/com/infosys/b4b/upload_fragment.java
@@ -1,64 +1,184 @@
 package com.infosys.b4b;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.ProgressDialog;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link upload_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.sql.Ref;
+import java.util.HashMap;
+import java.util.UUID;
+
 public class upload_fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final int RESULT_OK = -1;
+    private ImageView profilePic;
+    public Uri imageUri;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public upload_fragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment upload_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static upload_fragment newInstance(String param1, String param2) {
-        upload_fragment fragment = new upload_fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    TextInputEditText booktitle;
+    TextInputEditText bookdescribe;
+    TextInputEditText bookgenre;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
+
+    Button submitBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_upload_fragment, container, false);
+
+        String TAG="o";
+
+
+        profilePic = view.findViewById(R.id.iconIv);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+
+        booktitle = (TextInputEditText) view.findViewById(R.id.booktitle);
+        bookdescribe = (TextInputEditText) view.findViewById(R.id.bookdescribe);
+        bookgenre = (TextInputEditText) view.findViewById(R.id.bookgenre);
+        submitBtn = view.findViewById(R.id.submitBtn);
+
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choosePicture();
+
+            }
+        });
+
+
+
+
+
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Button submit_button = (Button) view;
+                //submit_button.setText("Submitted!");
+                long timestamp = System.currentTimeMillis();
+                String title = booktitle.getText().toString();
+                String description = bookdescribe.getText().toString();
+                String genre = bookgenre.getText().toString();
+                HashMap<String, Object> hashMap = new HashMap<>();
+                hashMap.put("title", ""+title);
+                hashMap.put("description", ""+description);
+                hashMap.put("genre", ""+genre);
+
+
+                if (title.isEmpty()){
+                    Toast.makeText(getContext(), "Please enter a title", Toast.LENGTH_SHORT).show();
+                }
+                else if (description.isEmpty()){
+                    Toast.makeText(getContext(), "Please enter a description", Toast.LENGTH_SHORT).show();
+                }
+                else if (genre.isEmpty()){
+                    Toast.makeText(getContext(), "Please enter a genre", Toast.LENGTH_SHORT).show();
+                }
+                else if (imageUri == null){
+                    Toast.makeText(getContext(), "Please pick an image", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getContext(), "Book added successfully", Toast.LENGTH_SHORT).show();
+                    FirebaseDatabase.getInstance("https://book4book-862cd-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("Books").push().setValue(hashMap);
+                    DatabaseReference reference = FirebaseDatabase.getInstance("https://book4book-862cd-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
+                    DatabaseReference reference2 = reference.child("Books");
+                    DatabaseReference reference3 = reference2.push();
+                    String postId = reference3.getKey();
+                    uploadPicture(postId);
+                }
+            }
+        });
+        return view;
     }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent.createChooser(intent,"Select image"),1);
+    }
+
+
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode==RESULT_OK  && data!=null){
+            imageUri = data.getData();
+            profilePic.setImageURI(imageUri);
+            //uploadPicture();
+        }
+    }
+
+    private void uploadPicture(String s) {
+
+        final ProgressDialog pd = new ProgressDialog(getContext());
+        pd.setTitle("uploading image...");
+        pd.show();
+
+
+        final String randomKey = UUID.randomUUID().toString();      //key of an image
+        StorageReference mountainsRef = storageReference.child("images/" + s);
+
+
+        mountainsRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        pd.dismiss();
+                        Snackbar.make(profilePic.findViewById(R.id.iconIv), "Image uploaded.", Snackbar.LENGTH_LONG).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        pd.dismiss();
+                        Toast.makeText(getContext().getApplicationContext(), "Failed to upload", Toast.LENGTH_LONG).show();}
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot tasksnapshot) {
+                        double progressPercent = (100.00 * tasksnapshot.getBytesTransferred() / tasksnapshot.getTotalByteCount());
+                        pd.setMessage("Progress:" +(int) progressPercent + "%");
+                    }
+                });
+
+    }
+
+
+
 }
