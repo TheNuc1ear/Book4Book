88c2485bb51aa32b7ca8b7d6b2816b17f433053b
TheNuc1ear <86493432+TheNuc1ear@users.noreply.github.com>
2022-03-29 23:22:14 +0800

    Revert "User profile"

--- app/src/androidTest/java/com/infosys/b4b/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/infosys/b4b/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.a1dproject;
+package com.infosys.b4b;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.a1dproject", appContext.getPackageName());
+        assertEquals("com.example.myfirstapp", appContext.getPackageName());
     }
 }
