908d8fe32bd904fbbdf0bcddda83e48198f47281
Leecz888 <90887690+Leecz888@users.noreply.github.com>
2022-04-08 23:26:11 +0800

    Added confirmation before deleting a listing

--- app/src/main/java/com/infosys/b4b/BookAdapter.java
+++ app/src/main/java/com/infosys/b4b/BookAdapter.java
@@ -1,6 +1,8 @@
 package com.infosys.b4b;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +13,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -36,16 +40,7 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.bookName.setText(listings.get(position).getNameOfBook());
         holder.bookDescription.setText(listings.get(position).getDescriptionOfBook());
-        holder.deletebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String listingId = listings.get(position).getListingId();
-                deleteBook(listingId);
-                Toast.makeText(view.getContext(), "Book deleted", Toast.LENGTH_SHORT).show();
-                listings.remove(position);
-                notifyDataSetChanged();
-            }
-        });
+
         StorageReference listingId = FirebaseStorage.getInstance().getReference().child("images/" + listings.get(position).getListingId());
         //holder.bookGenre.setText(model.getGenreOfBook());
         Task<Uri> url = FirebaseStorage.getInstance().getReference().child("images/" + listings.get(position).getListingId()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
@@ -53,6 +48,29 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
             @Override
             public void onSuccess(Uri uri) {
                 Glide.with(holder.bookImage.getContext()).load(uri).into(holder.bookImage);
+            }
+        });
+
+        //For deleting listing
+        holder.deletebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setMessage(R.string.alertDelete);
+                builder.setPositiveButton(R.string.confirmYes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String listingId = listings.get(position).getListingId();
+                        deleteBook(listingId);
+                        Toast.makeText(view.getContext(), "Book deleted", Toast.LENGTH_SHORT).show();
+                        listings.remove(position);
+                        notifyDataSetChanged();
+                    }
+                });
+
+                builder.setNegativeButton(R.string.confirmNo, null);
+                builder.show();
             }
         });
     }
